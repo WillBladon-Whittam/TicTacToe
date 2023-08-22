b71d2b5f4b2e085ebed8fe94ab76cb323e00a66f
WillBladon-Whittam <willbladon2.0@outlook.com>
2023-08-22 23:43:14 +0100

    Added minimax algorithm for AI
    Added the minimax algorithm to search the best moves for the computer. This is currently not finished as the computer doesn't always play the best moves. This is because the depth is currently set to 2. It would need to be set to 9 (Maximum number of moves to look in the future) to optimise the AI, but currently there are some issues where it seems to play worse with a higher depth. I think this is something to do with how I'm storing the index for the best position when it is found - but this needs looking into.

--- Headers/game.h
+++ Headers/game.h
@@ -9,12 +9,18 @@ public:
     void Update();
     void checkWon();
     void resetGame();
+    float minimax(string position[], int depth, bool maximizingPlayer);
+    float bestMove(string position[]);
 
-    inline static string grid_toggle[9] = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+    inline static string grid_toggle[9] = {"X", " ", "O", " ", " ", " ", " ", " ", " "};
 
     inline static bool placed = false;
 
     inline static int cross_wins = 0;
     inline static int nought_win = 0;
+
+private:
+    int bestIndexPos;
+
 
 };
--- SourceCode/game.cpp
+++ SourceCode/game.cpp
@@ -80,6 +80,87 @@ void Game::checkWon() {
     }
 }
 
+float Game::bestMove(string position[]) {
+    // 8 winning scenarios - 3 down on each row, 3 across on each row, 3 in a row on each 2 diagonals
+    // check downwards
+    for (int i=0; i < 9; i+=3) {
+        if (position[i] == "X" and position[i+1] == "X" and position[i+2] == "X") {
+            return 1;
+
+        } else if (position[i] == "O" and position[i+1] == "O" and position[i+2] == "O") {
+            return -1;
+        }
+    }
+
+    // check horizontal
+    for (int i=0; i < 9; i+=1) {
+        if (position[i] == "X" and position[i+3] == "X" and position[i+6] == "X") {
+            return 1;
+        } else if (position[i] == "O" and position[i+3] == "O" and position[i+6] == "O") {
+            return -1;
+        }
+    }
+    // check diagonals
+    if (position[0] == "X" and position[4] == "X" and position[8] == "X") {
+        return 1;
+    } else if (position[0] == "O" and position[4] == "O" and position[8] == "O") {
+        return -1;
+    }
+    if (position[2] == "X" and position[4] == "X" and position[6] == "X") {
+        return 1;
+    } else if (position[2] == "O" and position[4] == "O" and position[6] == "O") {
+        return -1;
+    }
+    return 0;
+}
+
+float Game::minimax(string position[], int depth, bool maximizingPlayer) {
+    // cout << "depth = " << depth << "\n";
+    // cout << "bestMove = " << bestMove(position) << "\n";
+    // cout << "bestIndex = " << bestIndexPos << "\n";
+    // cout << position[0] << " " << position[3] << " " << position[6] << "\n";
+    // cout << position[1] << " " << position[4] << " " << position[7] << "\n";
+    // cout << position[2] << " " << position[5] << " " << position[8] << "\n";
+    // cout << "\n";
+    if (depth == 0 or bestMove(position) != 0) {
+        float eval = bestMove(position);
+        return eval;
+    }
+
+    if (maximizingPlayer) {
+        float maxEval = -1000;
+        for (int i=0; i < 9; i++) {
+            if (position[i] == " ") {
+                position[i] = "X";
+                float eval = minimax(position, depth-1, false);
+                position[i] = " ";
+                if (eval > maxEval) {
+                    // cout << "maxEval before = " << maxEval << "\n";
+                    maxEval = eval;
+                    // cout << "maxEval after = " << maxEval << "\n";
+                }
+            }
+        }
+        return maxEval;
+    } else {
+        float minEval = 1000; 
+        for (int i=0; i < 9; i++) {
+            if (position[i] == " ") {
+                position[i] = "O";
+                int eval = minimax(position, depth-1, true);
+                position[i] = " ";
+                if (eval < minEval) {
+                    // cout << "minEval before = " << minEval << "\n";
+                    minEval = eval;
+                    // cout << "minEval after = " << minEval << "\n";
+                    bestIndexPos = i;
+                }
+            }
+        }
+        return minEval;
+    }
+}
+
 void Game::Update() {
     // Has player placed ?
     // If yes -> Place random noght for now, add a bot to generate a better move.
@@ -95,12 +176,14 @@ void Game::Update() {
 
         // Selects a random
         this_thread::sleep_for(chrono::milliseconds(1000));
+
         if (index_values.size() == 0) {
             cout << "Game Draw";
             resetGame();
         } else {
-            int random = rand() % index_values.size();
-            grid_toggle[index_values[random]] = "O";
+            minimax(grid_toggle, 2, false);
+            cout << "FINAL INDEX = " << bestIndexPos;
+            grid_toggle[bestIndexPos] = "O";
             placed = false;
 
             // Check if game has been won.
